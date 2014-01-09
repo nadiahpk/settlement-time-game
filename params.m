d993b61f02aaebc2549141262a2c1c5f66fcf885
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2014-01-09 18:17:00 +0100

    Whitespace OCD.

--- params.m
+++ params.m
@@ -2,7 +2,7 @@
 p.sA=0.6; % Adult survival (male, has refs)
 p.v=1;
 p.R=2.25; % Reproduction rate (has refs)
-p.TG=5; 
+p.TG=5;
 p.TB=10;
 p.lambdaG = 0.2;
 p.lambdaB = 2.2;
@@ -15,7 +15,7 @@ p.sJ = .35;
 % NOTE THIS MAY NEED TO BE RECALCULATED so use update_sBsG.m
 % If only there was a way to define it always as the product
 p.sB=p.fB*p.sJ; % Offspring survival bad territory
-p.sG=p.fG*p.sJ; 
+p.sG=p.fG*p.sJ;
 
 % Info about default params:
 % mu = 0.58899
